ab0112c5092086eac11409865501df487a045bbf
OhJay <ollejardbrink@gmail.com>
2025-04-02 16:25:11 +0200

    Last for the day, bug fixing last commit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import word_counter
+from stats import get_num_words
 
 def get_book_text(file_path):
     with open(file_path) as content:
@@ -6,7 +6,9 @@ def get_book_text(file_path):
 
 
 def main():
-    print(word_counter("./books/frankenstein.txt"))
-    #print(get_book_text("./books/frankenstein.txt"))
+    #text stores data for interaction with word_count
+    text = (get_book_text("./books/frankenstein.txt"))
+    word_count = get_num_words(text)
+    print(f"{word_count} words found in the document")
     
 main()
--- stats.py
+++ stats.py
@@ -1,5 +1,3 @@
-def word_counter(file_path):
-    with open(file_path) as content:
-        text = content.read()
-        num_words = len(text.split())
-    return f"{num_words} words found in the document"
+def get_num_words(content):
+        return len(content.split())
+        
