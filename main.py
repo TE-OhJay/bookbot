7e0ff24bf933b428bf67b4f02a785e723ff75f5f
OhJay <ollejardbrink@gmail.com>
2025-04-03 14:15:28 +0200

    Added more functions for analysis, prints report

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words
+from stats import get_num_words, count_characters, sort_characters
 
 def get_book_text(file_path):
     with open(file_path) as content:
@@ -6,9 +6,32 @@ def get_book_text(file_path):
 
 
 def main():
+    
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    
     #text stores data for interaction with word_count
     text = (get_book_text("./books/frankenstein.txt"))
-    word_count = get_num_words(text)
-    print(f"{word_count} words found in the document")
     
+    
+    print("----------- Word Count ----------")
+    #word_count counts the number of words in text.
+    word_count = get_num_words(text)
+    print(f"Found {word_count} total words")
+    
+    
+    
+    print("--------- Character Count -------")
+    #character_count simply counts all the characters in text, including symbols and spaces.
+    character_count = count_characters(text)
+    #character_sort simply sorts the character_count by nr. of appearance
+    character_sort = sort_characters(character_count)
+
+    for entry in character_sort:
+        if entry["character"].isalpha():
+            print(f"{entry["character"]}: {entry["count"]}")
+    
+    print("============= END ===============")
+
+
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,19 @@
 def get_num_words(content):
         return len(content.split())
-        
+
+
+def count_characters(input_text):
+        text = input_text.lower()
+        character_count = {}
+        for char in text:
+                if char in character_count:
+                        character_count[char] += 1
+                else:
+                        character_count[char] = 1
+        return character_count
+
+def sort_characters(big_dict):
+        character_list = [{"character": char, "count": count} for char, count in big_dict.items()]
+        character_list.sort(key=lambda x: x["count"], reverse=True)
+
+        return character_list
